c0ac0aa5b074b9b4d6cea961e1b82d11fc2cf58c
Kunta <30839398+Kuntapaliwal@users.noreply.github.com>
2019-04-07 20:16:37 +0530

    Update os_project4.c

--- os_project4.c
+++ os_project4.c
@@ -2,47 +2,45 @@
 #include<unistd.h>
 struct resources
 {
-	int p;
-	int pp;
-	int q_pp;
+	int pen;
+	int paper;
+	int q_paper;
 	int allot_id;
 
 };
 struct resource_count{
 	
-	int p_count ;
-	int pp_count ;
-	int q_pp_count ;
+	int pen_count ;
+	int paper_count ;
+	int q_paper_count ;
 }; 
 
 struct resource_count c[3];
 
-void allot(struct resource r)
+void allot(struct resources r)
 {
-	
-			
 	if(r.allot_id == 0)
 	{
-		r.p=1;
+		r.pen=1;
 	}
 	else if(r.allot_id == 1)
 	{
-		r.pp=1;
+		r.paper=1;
 	}
 	else if(r.allot_id == 2)
 	{
-		r.q_pp=1;
+		r.q_paper=1;
 	}
 	
 }
 int main()
 {
-	struct resource r[3];
+	struct resources s[3];
 	int i,j,count=0;
 	int p[20],bt[20], su[20], wt[20],tat[20], k, n, temp;
 	float wtavg, tatavg;
 	printf("************************we have 1 teacher process and 3 student process******************************\n");
-	printf("\t\n");
+	printf("\n");
 	for(i=0;i<4;i++)
 	{
 		p[i]=1;
@@ -59,68 +57,65 @@ int main()
 	}
 	for(i=0;i<3;i++)
 	{
-		r[i].allot_id=i;
-		allot(r[i]);
+		s[i].allot_id=i;
+		allot(s[i]);
 	}
 		printf("\nResources alloted to students are:");
 	for(i=0;i<3;i++)
 	{
-		if(r[i].p == 1)
+		if(s[i].pen == 1)
 		printf("\nResources alloted to student %d are:pen ",i+1);
 		
-		if(r[i].pp==1)
-		printf("\nResources alloted to student %d are:paper ",i);
+		if(s[i].paper == 1)
+		printf("\nResources alloted to student %d are:paper ",i+1);
 		
-		if(r[i].q_pp == 1)
+		if(s[i].q_paper == 1)
 		printf("\nResources alloted to student %d are:question paper ",i+1);
 	}
 		while(count != 3)
 		{
 			for(i=0;i<3;i++)
 			{
-				switch{
-					case 0:
-				
-					c[i].pp_count=1;
-					c[i].p_count=1;
+				if(i=0)
+				{
+					c[i].paper_count=1;
+					c[i].pen_count=1;
 					for(j=0;j<3;j++)
 					{
-						if(s[j].q_pp==1)
+						if(s[j].q_paper==1)
 						{
-						printf("\nStudent %d has completed his job !",j+1);
+						printf("\nStudent %d has completed his job !",i+1);
 						count++;
 						}
 					}
-				break;
-					case 1:
-			
-				c[i].q_pp_count=1;
-				c[i].p_count=1;
+				}
+			 if(i=1)
+			{
+				c[i].q_paper_count=1;
+				c[i].pen_count=1;
 				for(j=0;j<3;j++)
 				{
-					if(r[j].pp==1)
+					if(s[j].paper==1)
 					{
-						printf("\nStudent %d has completed his job !",j+1);
+						printf("\nStudent %d has completed his job !",i+1);
 						count++;
 					}
 				}
-			break;
+			}
 		
-					case 2:
-			
-				c[i].pp_count=1;
-				c[i].q_pp_count=1;
+			if(i=2)
+			{
+				c[i].paper_count=1;
+				c[i].q_paper_count=1;
 				for(j=0;j<3;j++)
 				{
-					if(r[j].p==1)
+					if(s[j].pen==1)
 					{
-						printf("\nStudent %d has completed his job !",j+1);
+						printf("\nStudent %d has completed his job !",i+1);
 						count++;
 					}
 				}
-			break:
-				}
-						
+			}
 	}	}	
 	for(i=0;i<4;i++)
 	{
@@ -151,7 +146,7 @@ int main()
 		wtavg = wtavg + wt[i];
 		tatavg = tatavg + tat[i];
 	}
-	printf("\nPROCESS\t\t   BURST TIME\t\t\tWAITING TIME\t\t TURNAROUND TIME");
+	printf("\nPROCESS\t\t   BURST TIME\tWAITING TIME\t\t TURNAROUND TIME");
 	
 	for(i=0;i<4;i++)
 	{
